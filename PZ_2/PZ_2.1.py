0d20bc2557967f952ea72663968d2a81fd37c34b
ThisVladimir <vladimirmilashich94@gmail.com>
2025-09-25 18:35:52 +0300

    initial commit

--- PZ_2/PZ_2.1.py
+++ PZ_2/PZ_2.1.py
@@ -1,1 +1,7 @@
-print("chjc")
+try:
+    segment1 = int(input("Введите длину первого отрезка:"))
+    segment2 = int(input("Введите длину второго отрезка(меньше первого отрезка):"))
+    remainder = segment1 % segment2
+    print("Незанятая часть отрезка А:",remainder)
+except ValueError:
+    print("Неправильный ввод данных. Введите число.")
